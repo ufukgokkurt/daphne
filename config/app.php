a664b0d94812185f06e9e7a11f639f9adb2e93ea
vagrant <ufuk.gokkurt@gmail.com>
2018-02-15 18:56:40 +0000

    install Sentinel packages

--- config/app.php
+++ config/app.php
@@ -154,7 +154,7 @@ return [
          * Eklediklerim
          */
         Collective\Html\HtmlServiceProvider::class,
-
+        Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -217,7 +217,9 @@ return [
          */
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-
+        'Activation' => Cartalyst\Sentinel\Laravel\Facades\Activation::class,
+        'Reminder'   => Cartalyst\Sentinel\Laravel\Facades\Reminder::class,
+        'Sentinel'   => Cartalyst\Sentinel\Laravel\Facades\Sentinel::class,
     ],
 
 ];
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -46,6 +46,8 @@ return array(
     'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'Cron\\' => array($vendorDir . '/dragonmantank/cron-expression/src/Cron'),
     'Collective\\Html\\' => array($vendorDir . '/laravelcollective/html/src'),
+    'Cartalyst\\Support\\' => array($vendorDir . '/cartalyst/support/src'),
+    'Cartalyst\\Sentinel\\' => array($vendorDir . '/cartalyst/sentinel/src'),
     'Carbon\\' => array($vendorDir . '/nesbot/carbon/src/Carbon'),
     'App\\' => array($baseDir . '/app'),
 );
